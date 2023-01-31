718afe99d1c895c2613ba6731d1f9087bbfc9de1
nafiurrashid <nafiurrashid@gmail.com>
2023-01-31 17:42:17 +0600

    Problem 03: finding max or min

--- Adding_sum_from_array.js
+++ Adding_sum_from_array.js
@@ -2,27 +2,24 @@
 //you have been provided with an array of numbers
 //return their sum
 
-
 //Solve 01: using for loop
 function sum(arr) {
-    let result = 0;
-    for (let i = 0; i <arr.length; i++) {
-      result = result + arr[i];
-    }
-    return result
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result = result + arr[i];
   }
-  
-  console.log(sum([1, 2, 3]));
+  return result;
+}
 
+console.log(sum([1, 2, 3]));
 
 //Solve 02: using for(i of array)
-  function add(arr) {
-    let result = 0;
-    for (let num of arr) {
-      result += num;
-    }
-    return result
+function add(arr) {
+  let result = 0;
+  for (let num of arr) {
+    result += num;
   }
-  
-  console.log(add([1, 2, 3]));
-  
+  return result;
+}
+
+console.log(add([1, 2, 3]));
--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,31 +1,28 @@
-//Write a problem that prints  from 1 to 100. 
+//Write a problem that prints  from 1 to 100.
 //But for multiples of three print "Fizz" instead of the number
 // and for multiples of five print "BUZZ".
 // For numbers which are Multiples of both three and five prints "FizzBuzz"
 
-function fizzBuzz(num){
-    let result = ''
-    for(let i=1; i<=num;i++){
-        //console.log(i)
-        if(i%3===0 && i%5===0){
-            result+='FizzBuzz \n'
-            //console.log("fizzBuzz")
-        }
-        else if(i%3===0){
-            result+='Fizz \n'
-            //console.log("Fizz")
-        }
-        else if(i%5===0){
-            result+='Buzz \n'
-            //console.log("Buzz")
-        }
-        else{
-            //console.log(i)
-            result+= i +"\n"
-        }
+function fizzBuzz(num) {
+  let result = "";
+  for (let i = 1; i <= num; i++) {
+    //console.log(i)
+    if (i % 3 === 0 && i % 5 === 0) {
+      result += "FizzBuzz \n";
+      //console.log("fizzBuzz")
+    } else if (i % 3 === 0) {
+      result += "Fizz \n";
+      //console.log("Fizz")
+    } else if (i % 5 === 0) {
+      result += "Buzz \n";
+      //console.log("Buzz")
+    } else {
+      //console.log(i)
+      result += i + "\n";
     }
-    return result
-    
+  }
+  return result;
 }
 
-console.log(fizzBuzz(100))
+console.log(fizzBuzz(100));
+
