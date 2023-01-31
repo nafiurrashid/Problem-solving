99fd87f3fc1f8ddf1092b4253ddccb02da189c04
nafiurrashid <nafiurrashid@gmail.com>
2023-01-31 17:08:17 +0600

    Problem 02: sum of array

---
+++ Adding_sum_from_array.js
@@ -0,0 +1,28 @@
+//adding sum from an array
+//you have been provided with an array of numbers
+//return their sum
+
+
+//Solve 01: using for loop
+function sum(arr) {
+    let result = 0;
+    for (let i = 0; i <arr.length; i++) {
+      result = result + arr[i];
+    }
+    return result
+  }
+  
+  console.log(sum([1, 2, 3]));
+
+
+//Solve 02: using for(i of array)
+  function add(arr) {
+    let result = 0;
+    for (let num of arr) {
+      result += num;
+    }
+    return result
+  }
+  
+  console.log(add([1, 2, 3]));
+  
