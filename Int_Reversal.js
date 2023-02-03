595ab1a6c969df805abf39058ad07bd2998cf5e9
nafiurrashid <nafiurrashid@gmail.com>
2023-02-03 20:37:02 +0600

    Int Reversal solved

---
+++ Int_Reversal.js
@@ -0,0 +1,13 @@
+// input: Number (eg:15)
+// output: reversed number (eg:51)
+
+
+function reverseInt(num){
+    let reverseNum=num.toString().split('').reverse().join('')
+    if (reverseNum.endsWith('-')){
+        reverseNum = '-'+reverseNum.slice(0,reverseNum.length-1)
+    }
+    console.log(reverseNum)
+    return Number(reverseNum)  
+}
+reverseInt(-90)
---
+++ String_revarsal.js
@@ -0,0 +1,10 @@
+//input: a string eg: apple
+//output: reversed string eg: elppa
+
+function reverse(str){
+    for(let i=str.length-1;i>=0;i--){
+        
+    }
+}
+
+function reverse("apple")
