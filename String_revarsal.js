ced79af8afa067ac98b410f31b62de9e590b3b96
nafiurrashid <nafiurrashid@gmail.com>
2023-02-03 21:25:08 +0600

    String Reversal solved

--- String_revarsal.js
+++ String_revarsal.js
@@ -1,10 +1,32 @@
 //input: a string eg: apple
 //output: reversed string eg: elppa
 
-function reverse(str){
-    for(let i=str.length-1;i>=0;i--){
-        
-    }
+//sove 01
+function reverse(str) {
+  for (let i = str.length - 1; i >= 0; i--) {}
 }
 
-function reverse("apple")
+reverse("apple");
+
+//solve 02
+
+function reversedString(str) {
+  let revStr = "";
+  for (let char of str) {
+    revStr = char + revStr;
+  }
+
+  return revStr;
+}
+
+reversedString("apple");
+console.log(reversedString("apple"));
+
+//solve 03
+
+function reverse03(str) {
+  let b = str.split("").reverse().join("");
+  return b;
+}
+
+reverse03("apple");
